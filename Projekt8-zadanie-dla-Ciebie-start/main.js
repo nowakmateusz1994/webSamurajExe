e2e3079a06b805606e844cf018219c1d23df4036
Mateusz Nowak <39436410+nowakmateusz1994@users.noreply.github.com>
2019-02-10 18:26:54 +0100

    prosty typing w JS od samuraja

---
+++ Projekt8-zadanie-dla-Ciebie-start/main.js
@@ -0,0 +1,52 @@
+// http://websamuraj.pl/examples/js/projekt8/
+// Uzyskaj efekt jak pod tym linkiem 
+// Użyj setTimeout zamiast setInterval,
+// Opóźniej start pisanie każdego słowa
+// Uwaga: Zadanie bardzo trudne
+
+const spanText = document.querySelector('.text');
+const spnCursor = document.querySelector('.cursor');
+const txt = ['tekst1', 'tekst2 tekst2', 'tekst3 tekst3 tekst3']
+let letterIndex = 0;
+let wordIndex = 0;
+
+// Implementacja
+const addLetter = () => {
+
+
+    let word = txt[wordIndex];
+
+    spanText.textContent += word[letterIndex];
+    letterIndex++;
+
+    if (letterIndex < word.length) {
+        setTimeout(addLetter, 200);
+
+    } else if (letterIndex >= word.length) {
+        letterIndex = 0;
+        wordIndex++;
+        if (wordIndex >= txt.length) {
+            wordIndex = 0;
+        }
+        setTimeout(function () {
+            spanText.textContent = '';
+            setTimeout(addLetter, 500)
+        }, 500)
+
+    }
+
+
+};
+
+
+
+
+
+addLetter(); //pierwsze wywołanie
+
+
+// Animacja kursora (zostaw bez zmian)
+const cursorAnimation = () => {
+    spnCursor.classList.toggle('active');
+}
+setInterval(cursorAnimation, 400);
